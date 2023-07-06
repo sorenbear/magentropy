9f1fb4b36115b9f44cca70084495c5e90cd49417
Soren Bear <soren.bear@gmail.com>
2023-07-06 09:40:35 -0700

    Update email and copyright year

--- src/_meta/__init__.py
+++ src/_meta/__init__.py
@@ -3,8 +3,8 @@
 # keep consistent with pyproject.toml
 __project__ = 'MagentroPy'
 __author__ = 'Soren Bear'
-__email__ = 'sbear@ucsb.edu'
-__copyright__ = f"2022, {__author__}"
+__email__ = 'sorenbear@berkeley.edu'
+__copyright__ = f'2022-2023, {__author__}'
 __license__ = 'MIT'
 __version__= '0.1.5'
 __repo_url__ = 'https://github.com/sorenbear/magentropy'
